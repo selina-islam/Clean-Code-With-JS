e796e67d76088c6e415e22bfc31cc5ba90252b20
Selina <digitalselina@gmail.com>
2025-04-15 21:44:07 +0600

    append Child

---
+++ Advanced/DOM/appendChild/app.js
@@ -0,0 +1,5 @@
+const Frist = document.querySelector("#first-list");
+const Second = document.querySelector("#second-list");
+
+let FirstItem = Frist.firstElementChild // first child select
+Second.appendChild(FirstItem)
