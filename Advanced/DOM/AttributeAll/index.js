bad3c111a26d33d578ca56db4c7546708a81fc0f
Selina <digitalselina@gmail.com>
2025-04-17 17:31:18 +0600

    Attribute methods

---
+++ Advanced/DOM/AttributeAll/index.js
@@ -0,0 +1,33 @@
+
+
+const element = document.getElementById('js')
+
+// getAttribute()
+element.getAttribute('target')
+console.log(element)
+
+
+// setAttribute()
+element.setAttribute('name', 'Selina')
+console.log(element)
+
+element.setAttribute('disable')
+console.log(element)
+
+
+// removeAttribute()
+
+element.removeAttribute('target')
+console.log(element)
+
+// hasAttribute()
+const ss=element.hasAttribute('name')
+console.log(ss)  // true
+
+
+
+
+
+
+
+
